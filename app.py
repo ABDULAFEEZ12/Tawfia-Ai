e66e00f6cf460df13511e549fc910289a427a9a1
ABDULAFEEZ12 <tellaafeezadewale@gmail.com>
2025-12-31 01:33:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -891,11 +891,4 @@ def debug_rooms():
 # Run Server
 # ============================================
 
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
